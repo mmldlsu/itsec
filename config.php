1ef4afea26f0a9e7a43762b882f7ccbc3dbad26a
kmcasingal <113958738+kmcasingal@users.noreply.github.com>
2024-08-08 21:49:26 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,63 @@
+<?php
+
+if (!defined('DEBUG')) {
+    define('DEBUG', true); // Set to false to disable debug mode
+}
+
+// Set error reporting based on debug mode
+if (DEBUG) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+} else {
+    error_reporting(0);
+    ini_set('display_errors', 0);
+}
+
+// Global Error Handler
+function globalErrorHandler($errno, $errstr, $errfile, $errline)
+{
+    $errorMsg = "Error [{$errno}]: {$errstr} in {$errfile} on line {$errline}";
+
+    if (DEBUG) {
+        echo "<b>Debug Error:</b> {$errorMsg}<br>";
+        echo "<pre>" . print_r(debug_backtrace(), true) . "</pre><br>";
+    } else {
+        error_log($errorMsg, 3, 'error_log.txt');
+        echo 'An error occurred.';
+    }
+
+    return true;
+}
+
+// Global Exception Handler
+function globalExceptionHandler($exception)
+{
+    $exceptionMsg = "Uncaught Exception: " . $exception->getMessage() . " in " . $exception->getFile() . " on line " . $exception->getLine();
+
+    if (DEBUG) {
+        echo "<b>Debug Error:</b> {$exceptionMsg}<br>";
+        echo "<pre>" . $exception->getTraceAsString() . "</pre><br>";
+    } else {
+        error_log($exceptionMsg, 3, 'error_log.txt');
+        echo 'An error occurred.';
+    }
+
+    exit();
+}
+
+// Shutdown function to catch fatal errors
+function shutdownFunction()
+{
+    $error = error_get_last();
+    if ($error !== null) {
+        globalErrorHandler($error['type'], $error['message'], $error['file'], $error['line']);
+    }
+}
+
+// Register global handlers
+set_error_handler("globalErrorHandler");
+set_exception_handler("globalExceptionHandler");
+register_shutdown_function('shutdownFunction');
+
+
+?>
