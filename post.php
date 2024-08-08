6beaf4ba7cec9f0334ab60a9ad2b2a6730140f39
kmcasingal <113958738+kmcasingal@users.noreply.github.com>
2024-08-08 22:47:42 +0800

    Update post.php

--- post.php
+++ post.php
@@ -2,6 +2,7 @@
 session_start();
 include 'connect.php'; // Ensure connection is included
 include 'logfunctions.php';
+include 'config.php';
 
 function createPost($userEmail, $content, $numericInput1, $numericInput2)
 {
